3ea128bd2571ede772d5d4b245dfe336499942d9
jsvhqr <johanpetersn@yahoo.com>
2016-07-03 21:37:02 +0200

    first commit

---
+++ AllianceFanshits/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllianceFanshits.Startup))]
+namespace AllianceFanshits
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
